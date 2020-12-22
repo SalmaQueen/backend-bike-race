915cf62d6414c9a22d8c6283a889f9a23f0c802b
salmaQueen <salmawasee9@gmail.com>
2020-12-22 19:57:35 +0300

    validation for length of characters in slogan

--- app/models/slogan.rb
+++ app/models/slogan.rb
@@ -1,6 +1,9 @@
 class Slogan < ApplicationRecord
     validates :firstname, presence: true
     validates :lastname, presence: true
-    validates :email, presence: true
-    validates :slogan, presence: true
+    validates :email, presence: true 
+    validates_presence_of :slogan 
+    validates_length_of :slogan, :maximum=> 50
+    validates :email, uniqueness: true
+    validates :email, format: { with: /\S+@\S+\.\S+/, on: :create }
 end
--- spec/models/slogan_spec.rb
+++ spec/models/slogan_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Slogan, type: :model do
+
  context "validation test" do
 
       it "ensures first name presence" do
@@ -14,18 +15,46 @@ RSpec.describe Slogan, type: :model do
     end
 
     it "ensures email presence" do
-      slogan = Slogan.new(firstname:'First',email:'sample@gmail.com', slogan: 'sometext').save
+      slogan = Slogan.new(firstname:'First',lastname:'Last', slogan: 'sometext').save
       expect(slogan).to eq(false)
     end
 
     it "ensures slogan presence" do
-      slogan = Slogan.new(firstname:'First',email:'sample@gmail.com', slogan: 'sometext').save
+      slogan = Slogan.new(firstname:'First',lastname:'Last', email:'sample@gmail.com').save
       expect(slogan).to eq(false)
+
+
     end
+    # it { should ensure_length_of(:slogan).is_at_most(50) }
+
+  # it "slogan should not be more than 50" do
+  #   slogan = Slogan.new(firstname:'First',lastname:'Last',slogan:"sometext" email:'sample@gmail.com').save
+
+  #   expect(slogan).is_at_most(50)
+  # end
+
+  describe "length validation" do
+  it "should not allow a slogan more than 5o characters" do
+    @slogan = Slogan.create(firstname:'First',lastname:'Last', slogan: "sometext", email:'sample@gmail.com')
+    @slogan.slogan= "j" * 51
+    expect(@slogan).to_not be_valid
+  end
+  end
+    
+
+  
 
     it "should save successfully" do
       slogan = Slogan.new(firstname:'First',lastname:'Last',email:'sample@gmail.com', slogan: 'sometext').save
       expect(slogan).to eq(true)
     end
+
+
+    context 'when email is not unique' do
+      before { described_class.create!(firstname:'First',lastname:'Last',slogan: 'sometext', email:'sample@gmail.com') }
+      it {expect(subject).to be_invalid}
+    end
   end
+
+
 end
